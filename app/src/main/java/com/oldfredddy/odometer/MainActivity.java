93075bab4f3ada8b44d6cdbbc9b52104fb886ccc
Freddy <goblinblin@mail.ru>
2020-08-06 21:15:33 +0300

    Счетчик пройденного расстояния версия 1

--- app/src/main/java/com/oldfredddy/odometer/MainActivity.java
+++ app/src/main/java/com/oldfredddy/odometer/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -60,17 +63,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayDistance() {
         final TextView distanceView = findViewById(R.id.distance);
+        final TextView splitNum = findViewById(R.id.split);
         final Handler handler = new Handler();
+
         handler.post(new Runnable() {
             @Override
             public void run() {
                 double distance = 0.0;
+                Location startLocation = null;
 
                 if (bound && odometer != null) {
                     distance = odometer.getDistance();
+                    String splitNumber = splitNum.getText().toString();
+                    int splitNumInt = Integer.parseInt(splitNumber);
+                    distance = distance/splitNumInt;
                 }
-                BigDecimal result = new BigDecimal(distance);
-                String distanceStr = String.format(Locale.getDefault(), "%1$,.2f miles", distance);
+                String distanceStr = String.format(Locale.getDefault(), "%1.3f m", distance);
                 distanceView.setText(distanceStr);
                 //distanceView.setText((result.setScale(2,RoundingMode.HALF_EVEN)).toString());
                 handler.postDelayed(this, 1000);
